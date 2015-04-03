46bf2198aac7b6466607ac08390e859cc9d7679e
Lavinia Banu <lavinia-violeta.banu@my.fmi.unibuc.ro>
2015-04-03 21:05:12 +0300

    Adaugare Clase Comanda si ItemComanda
    Au fost adaugate clasele Comanda si ItemComanda, ce mapeaza tabela
    "Comanda" din baza de date.

---
+++ App_Code/DB_Classes/Comanda.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace db_mapping
+{
+
+    /// <summary>
+    /// Clasa Comanda mapeaza tabela "Comenzi" din baza de date.
+    /// Contine, in plus, un HashSet de ItemComanda, iar pretul se updateaza
+    /// la fiecare inserare de ItemComanda.
+    /// </summary>
+    public class Comanda
+    {
+        public Comanda()
+        {
+            lista_item_comanda_ = new HashSet<ItemComanda>();
+            pret_ = 0;
+        }
+
+        // Returneaza intreg continutul unui obiect de tip Comanda
+        // sub forma de String. Metoda folosita pentru testare.
+        public String toString()
+        {
+            String comanda = "";
+            String new_line = "\n";
+
+            foreach (ItemComanda item_comanda in lista_item_comanda_)
+            {
+                comanda += item_comanda.toString();
+                comanda += new_line;
+                comanda += "Id utilizator = " + id_user_ + new_line;
+                comanda += "Pret = " + pret_ + new_line;
+                comanda += "Data = " + date_time_ + new_line;
+            }
+
+            return comanda;
+        }
+
+        // Adauga un ItemComanda la Comanda.
+        // Pretul comenzii este updatat.
+        public void addItemComanda(ItemComanda item_comanda)
+        {
+            lista_item_comanda_.Add(item_comanda);
+            pret_ += item_comanda.getPret();
+        }
+
+        // Setteri.
+        public void setIdUser(int id_user)
+        {
+            id_user_ = id_user;
+        }
+
+        public void setDateTime(DateTime date_time)
+        {
+            date_time_ = date_time;
+        }
+
+        // Getteri.
+        public double getPret()
+        {
+            return pret_;
+        }
+
+        public DateTime getDateTime()
+        {
+            return date_time_;
+        }
+
+        public int getIdUser()
+        {
+            return id_user_;
+        }
+
+        private HashSet<ItemComanda> lista_item_comanda_;
+        private double pret_;
+        private DateTime date_time_;
+        private int id_user_;
+    }
+} // namespace
---
+++ App_Code/DB_Classes/ItemComanda.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace db_mapping
+{
+    /// <summary>
+    /// ItemComanda este wrapper peste clasa Preparat.
+    /// Un obiect de tip ItemComanda contine un singur obiect Preparat,
+    /// cantitatea in care acest preparat a fost comandat si pretul (total).
+    /// Toate modificarile asupra cantitatii vor updata si pretul.
+    /// </summary>
+    public class ItemComanda
+    {
+        // Constructorul necesita doar un preparat. Cantitatea poate fi
+        // omisa aici si setata apoi prin intermediul setCantitate.
+        public ItemComanda(Preparat preparat, int cantitate = 0)
+        {
+            preparat_ = preparat;
+            cantitate_ = cantitate;
+            pret_ = preparat_.getPret() * cantitate_;
+        }
+
+        // Seteaza o cantitate pentru obiectul ItemComanda
+        // si updateaza pretul.
+        public void setCantitate(int cantitate)
+        {
+            cantitate_ = cantitate;
+            pret_ = preparat_.getPret() * cantitate_;
+        }
+
+        // Returneaza intreg continutul unui obiect de tip ItemComanda
+        // sub forma de String. Metoda folosita pentru testare.
+        public String toString()
+        {
+            String item_comanda = "";
+            String new_line = "\n";
+            item_comanda += preparat_.toString();
+            item_comanda += new_line;
+            item_comanda += "Cantitate = " + cantitate_ + new_line;
+            item_comanda += "Pret item comanda = " + pret_ + new_line;
+
+            return item_comanda;
+        }        
+
+        // Getteri.
+        public Preparat getPreparat()
+        {
+            return preparat_;
+        }
+
+        public int getCantitate()
+        {
+            return cantitate_;
+        }
+
+        public double getPret()
+        {
+            return pret_;
+        }
+
+        private Preparat preparat_;
+        private int cantitate_;
+        private double pret_;
+    }
+} // namespace
