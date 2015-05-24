4aaf800146aef19a9898e0c26fbda51d69425744
Lavinia Banu <lavinia-violeta.banu@my.fmi.unibuc.ro>
2015-05-24 21:38:49 +0300

    Istoric comenzi si axe

---
+++ App_Code/DB_Classes/IstoricComenzi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace db_mapping
+{
+    /// <summary>
+    /// IstoricComenzi mapeaza istoricul de comenzi din BD ale unui utilizator.
+    /// </summary>
+    public class IstoricComenzi
+    {
+        public static int UninitializedInt = -100000;
+
+        public IstoricComenzi(int user_id = -100000)
+        {
+            id_user_ = user_id;
+            lista_item_comanda_ = new List<Comanda>();
+        }
+
+        public void addComanda(Comanda comanda)
+        {
+            lista_item_comanda_.Add(comanda);
+
+        }
+
+        // Getteri.
+        public int IdUser
+        {
+            get { return id_user_; }
+        }
+
+        public List<Comanda> ListaItemComanda
+        {
+            get { return lista_item_comanda_; }
+        }
+
+        private List<Comanda> lista_item_comanda_;
+        int id_user_;
+    }
+
+}
---
+++ App_Code/Rec_Classes/Axa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Axa din spatiul N-dimensional de preparate.
+/// Contine un id de preparat si scorul corespunzator lui.
+/// </summary>
+public class Axa
+{
+	public Axa(int id)
+	{
+        id_preparat_ = id;
+        scor_ = 0;
+	}
+
+    // Setteri si getteri.
+    public int IdPreparat
+    {
+        get { return id_preparat_; }
+    }
+
+    public double Scor
+    {
+        get { return scor_; }
+        set { scor_ = value; }
+    }
+
+    private double scor_;
+    private int id_preparat_;
+}
